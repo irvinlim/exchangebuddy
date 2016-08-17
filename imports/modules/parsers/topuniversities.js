3bfd7d6980169e4ab8f1b6f67466f1e3eeeb9168
Irvin Lim <irvinlimweiquan@gmail.com>
2016-08-18 04:44:37 +0800

    Added parser and methods to update fixtures for Universities

---
+++ imports/modules/parsers/topuniversities.js
@@ -0,0 +1,39 @@
+import { bulkCreateOrUpdate } from '../../api/universities/methods';
+
+export const parseJSON = (json) => {
+  const countryMapping = require('../../../data/sources/topuniversities/countryMapping.json');
+
+  return json.map(uni => {
+    let logoUrl = "",
+        linkUrl = "";
+
+    if (uni.logo) {
+      logoUrl = uni.logo.substr(10);
+      logoUrl = logoUrl.substr(0, logoUrl.indexOf("\""));
+    }
+
+    if (uni.link) {
+      linkUrl = uni.link.substr(9);
+      linkUrl = 'http://www.topuniversities.com' + linkUrl.substr(0, linkUrl.indexOf("\""));
+    }
+
+    return {
+      name: uni.title,
+      countryCode: countryMapping[uni.country_tid],
+      topUnisId: uni.cid,
+      logoUrl,
+      linkUrl,
+    };
+  });
+};
+
+export const updateUniversities = () => {
+  // Read input file
+  const inputJSON = require('../../../data/sources/topuniversities/299926.json');
+
+  // Update parsed JSON to database
+  const parsed = parseJSON(inputJSON);
+
+  // Add to database
+  bulkCreateOrUpdate(parsed);
+};
