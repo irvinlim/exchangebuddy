f83d5a1878228ccbc9fe132f938e6022b6ba33ed
Lam Chi Thanh <E0008973@u.nus.edu>
2016-08-19 00:05:49 +0800

    Working on Home Page

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import LoginButton from '../components/LoginButton';
+import * as ImagesHelper from '../../util/images';
+import * as IconsHelper from '../../util/icons';
+import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
+const welcomeStyle = { height: $(window).height(), backgroundColor: "darkslategray" }
 
 const Home = () => (
-  <div className="text-center">
-    <h2>Our App Name</h2>
-    <LoginButton />
+  <div className="text-center" style={welcomeStyle}>
+    <div id="welcome-header">
+
+      <div id="logo-image">
+        { ImagesHelper.makeScale(Meteor.settings.public.logoImageId, 99, "exchangebuddy-logo") }
+      </div>
+
+      <Grid>
+        <Row>
+          <Col xs={0} md={2}></Col>
+          <Col xs={12} md={8} id="welcome-header-title">
+            <h2 id="app-name">Exchange Buddy</h2>
+            <p id="app-description">Find other exchange students like yourself</p>
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+
+    <div id="welcome-body">
+      <LoginButton />
+    </div>
   </div>
 );
 
