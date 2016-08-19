db49cc6970ca4fba49a9accf84ca9113c20b9630
Irvin Lim <irvinlimweiquan@gmail.com>
2016-08-20 01:30:19 +0800

    Added grid for signup (temp)

--- imports/ui/pages/signup.js
+++ imports/ui/pages/signup.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Grid } from 'meteor/lifefilm:react-flexbox-grid';
 
 import SignupStepper from '../components/SignupStepper';
 
 const Signup = () => (
-  <div className="text-center">
-    <h2>Complete your profile</h2>
-    <SignupStepper />
-  </div>
+  <Grid>
+    <div className="text-center">
+      <h2>Complete your profile</h2>
+      <SignupStepper />
+    </div>
+  </Grid>
 );
 
 export default Signup;
