7cb2a610f31f494d1a5b77283877586e3d566e91
leon mak <leonmakansheng@gmail.com>
2016-08-19 10:30:39 +0800

    Added weather api calls to info page

--- imports/ui/components/Card/Weather.js
+++ imports/ui/components/Card/Weather.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
 import {Card, CardHeader, CardText} from 'material-ui/Card';
+import Loading from '../Loading';
 import FlatButton from 'material-ui/FlatButton';
 
 export default class Weather extends React.Component {
@@ -12,29 +13,35 @@ export default class Weather extends React.Component {
   }
 
   componentDidMount() {
-    // TODO: Query 3hr/ daily forecast api
-    this.setState({ forecast: {
-      "weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
-      "main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
-    }})
+    $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=Singapore&mode=json&units=metric&cnt=6&appid=" +
+      Meteor.settings.public.OpenWeatherMap.apiKey,
+      result => this.setState({ forecast: {
+          name: result.city.name,
+          morning: result.list[1],
+          noon: result.list[3],
+          evening: result.list[5]
+        }})
+    );
   }
 
   render() {
    return (
     <Card>
-      {this.state.forecast &&
-      <CardHeader
-        title="Weather"
-        subtitle={this.state.forecast.weather[0].main}
-        actAsExpander={true}
-        showExpandableButton={false} />
+      { this.state.forecast ?
+      <div>
+        <CardHeader
+          title={this.state.forecast.name}
+          actAsExpander={true}
+          showExpandableButton={false} />
+        <CardText expandable={true}>
+          Morning: {this.state.forecast.morning.weather[0].main},
+          Noon: {this.state.forecast.noon.weather[0].main},
+          Evening: {this.state.forecast.evening.weather[0].main}
+        </CardText>
+      </div>
+      :
+      <Loading />
       }
-      <CardText expandable={true}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-      </CardText>
     </Card>
     );
   }
