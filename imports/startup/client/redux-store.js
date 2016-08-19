8699603f560f74ad64704a5a62f01c945aac6b88
Irvin Lim <irvinlimweiquan@gmail.com>
2016-08-19 14:54:20 +0800

    Added redux-devtools-extension support, add meteor-node-stubs

--- imports/startup/client/redux-store.js
+++ imports/startup/client/redux-store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 import rootReducer from '../../client/reducers/rootReducer';
 
-const Store = createStore(rootReducer);
+const Store = createStore(rootReducer, window.devToolsExtension && window.devToolsExtension());
 
 export default Store;
