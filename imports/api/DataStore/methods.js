2a422261ac6e8f976ae88fbfd723d7b647f4f97d
Irvin Lim <irvinlimweiquan@gmail.com>
2016-08-23 17:32:46 +0800

    Typo, should be hours not minutes

--- imports/api/DataStore/methods.js
+++ imports/api/DataStore/methods.js
@@ -15,12 +15,12 @@ if (Meteor.isServer) {
         const newValues = { dataKey, dataValue: dataCallback(existingRow.value) };
 
         // Immediately upsert if no row exists/lastUpdated was too long ago.
-        if (!existingRow || moment(existingRow.updatedAt).isBefore(moment().subtract(intervalHours, 'minutes'))) {
-          timeTillUpdate = moment().add(intervalHours, 'minutes');
+        if (!existingRow || moment(existingRow.updatedAt).isBefore(moment().subtract(intervalHours, 'hours'))) {
+          timeTillUpdate = moment().add(intervalHours, 'hours');
           DataStore.upsert(newValues);
         } else {
           const lastUpdated = existingRow.updatedAt;
-          const nextUpdateTime = moment(lastUpdated).add(intervalHours, 'minutes');
+          const nextUpdateTime = moment(lastUpdated).add(intervalHours, 'hours');
           timeTillUpdate = nextUpdateTime.diff(moment());
         }
 
