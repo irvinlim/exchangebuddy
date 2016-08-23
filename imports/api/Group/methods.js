defcd7b4609334a80083ea4e889bd164f1157526
Irvin Lim <irvinlimweiquan@gmail.com>
2016-08-23 17:27:47 +0800

    Move user's groups/group's users methods to api/UserGroup instead

--- imports/api/Group/methods.js
+++ imports/api/Group/methods.js
@@ -20,48 +20,6 @@ if (Meteor.isServer) {
       });
     },
 
-    addUserToGroup(values) {
-      check(values, Object);
-
-      const { userId, exchangeUniName, exchangeUniYear, exchangeTerm } = values;
-      check(userId, Number);
-      check(exchangeUniName, String);
-      check(exchangeUniYear, Number);
-      check(exchangeTerm, String);
-
-      return University.findOne({ where: { name: exchangeUniName } }).then(function(result) {
-        if (!result)
-          throw new Meteor.Error("addUserToGroup.undefinedUniversity", "No such university found.");
-
-        const uni = result.get();
-        const values = { universityId: uni.id, year: exchangeUniYear, term: exchangeTerm };
-
-        return Group.findOrCreate({ where: values, defaults: values });
-      }).then(function(result) {
-        const group = result[0];
-
-        if (group)
-          return group.addUser(userId);
-        else
-          return false;
-      }).then(function(result) {
-        if (result)
-          return true;
-        else
-          return false;
-      });
-    },
-
-    getUserGroups(userId) {
-      check(userId, Number);
-
-      return User.findOne({ where: { id: userId } }).then(function(userResult) {
-        return userResult.getGroups();
-      }).then(function(result) {
-        return result.map(x => x.get({ plain: true }));
-      });
-    },
-
     getGroupFbEvents(countryId, uniCBDLatLng) {
       check(countryId, String);
       check(uniCBDLatLng, Array);
@@ -100,8 +58,6 @@ if (Meteor.isServer) {
           });
         }).catch(error => console.error(JSON.stringify(error)));
       }).catch(error => console.error(JSON.stringify(error)));
-
-
     },
 
     getGroupMuEvents(latLng, cityName, pageNumber) {
@@ -129,7 +85,7 @@ if (Meteor.isServer) {
       return eventsPromise.then(function(events) {
         return events;
       })
+    },
 
-    }
   });
 }
--- imports/api/index.js
+++ imports/api/index.js
@@ -14,6 +14,7 @@ import './User/methods';
 import './University/methods';
 import './Country/methods';
 import './Group/methods';
+import './UserGroup/methods';
 
 // Publications
 import './University/publications';
