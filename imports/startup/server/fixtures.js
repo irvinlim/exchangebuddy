caa7f864b1401f32c6457ecc11f1e15fd1e5d533
Irvin Lim <irvinlimweiquan@gmail.com>
2016-08-24 02:46:16 +0800

    TEMP: Update universities in fixtures after 5 seconds because of async

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -24,41 +24,46 @@ University.count({}).then(function(count) {
     updateUniversities();
 });
 
-// Add some universities
-University.findOne({
-  where: { name: "Singapore University of Technology and Design (SUTD)" }
-}).then(function(result) {
-  if (!result)
-    University.create({
-      name: "Singapore University of Technology and Design (SUTD)",
-      city: "Singapore",
-      countryCode: "SG",
-      logoImageId: 'logo-sutd-main_hphs2p',
-      bgImageId: 'sutd-slider-aeriel-view_uckqr6_cdfnan',
-      emailDomains: JSON.stringify([ 'sutd.edu', 'sutd.edu.sg' ]),
-      terms: JSON.stringify([ "Semester 1", "Semester 2", "Semester 3" ]),
-    });
-});
+// TEMP: Update universities after 5 sec because of async
+setTimeout(() => {
 
-// Add data for some universities
-University.update({
-  city: "Singapore",
-  emailDomains: JSON.stringify([ 'nus.edu.sg', 'u.nus.edu' ]),
-  terms: JSON.stringify([ "Semester 1", "Semester 2", "Special Term Part 1", "Special Term Part 2" ]),
-}, { where: { name: "National University of Singapore (NUS)" } });
+  // Add some universities
+  University.findOne({
+    where: { name: "Singapore University of Technology and Design (SUTD)" }
+  }).then(function(result) {
+    if (!result)
+      University.create({
+        name: "Singapore University of Technology and Design (SUTD)",
+        city: "Singapore",
+        countryCode: "SG",
+        logoImageId: 'logo-sutd-main_hphs2p',
+        bgImageId: 'sutd-slider-aeriel-view_uckqr6_cdfnan',
+        emailDomains: JSON.stringify([ 'sutd.edu', 'sutd.edu.sg' ]),
+        terms: JSON.stringify([ "Semester 1", "Semester 2", "Semester 3" ]),
+      });
+  });
 
-University.update({
-  city: "Singapore",
-  emailDomains: JSON.stringify([ 'ntu.edu', 'ntu.edu.sg' ]),
-  terms: JSON.stringify([ "Semester 1", "Semester 2", "Special Term I", "Special Term II" ]),
-}, { where: { name: "Nanyang Technological University, Singapore (NTU)" } })
+  // Add data for some universities
+  University.update({
+    city: "Singapore",
+    emailDomains: JSON.stringify([ 'nus.edu.sg', 'u.nus.edu' ]),
+    terms: JSON.stringify([ "Semester 1", "Semester 2", "Special Term Part 1", "Special Term Part 2" ]),
+  }, { where: { name: "National University of Singapore (NUS)" } });
 
-University.update({
-  name: "Singapore Management University (SMU)",
-  city: "Singapore",
-  emailDomains: JSON.stringify([ 'smu.edu', 'smu.edu.sg' ]),
-  terms: JSON.stringify([ "Semester 1", "Semester 2" ]),
-}, { where: { name: "Singapore Management University" } });
+  University.update({
+    city: "Singapore",
+    emailDomains: JSON.stringify([ 'ntu.edu', 'ntu.edu.sg' ]),
+    terms: JSON.stringify([ "Semester 1", "Semester 2", "Special Term I", "Special Term II" ]),
+  }, { where: { name: "Nanyang Technological University, Singapore (NTU)" } })
+
+  University.update({
+    name: "Singapore Management University (SMU)",
+    city: "Singapore",
+    emailDomains: JSON.stringify([ 'smu.edu', 'smu.edu.sg' ]),
+    terms: JSON.stringify([ "Semester 1", "Semester 2" ]),
+  }, { where: { name: "Singapore Management University" } });
+
+}, 5000);
 
 // Add currency exchange rates data
 // TODO: Set a cron job to update exchange rates
