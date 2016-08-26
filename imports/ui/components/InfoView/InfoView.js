57dc3c8a4a55e91c6b5d08bccec2f252bb93e7e4
leon mak <leonmakansheng@gmail.com>
2016-08-26 21:33:41 +0800

    Edited info view page

--- imports/ui/components/InfoView/InfoView.js
+++ imports/ui/components/InfoView/InfoView.js
@@ -1,4 +1,14 @@
 import React, {Component, PropTypes} from 'react';
+import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
+import Paper from 'material-ui/Paper';
+import { GridList, GridTile } from 'material-ui/GridList';
+import RaisedButton from 'material-ui/RaisedButton';
+import * as ImagesHelper from '../../../util/images';
+import Markdown from 'react-markdown';
+
+const infoDisplayUrl = ImagesHelper.getUrl("uhllazfghy8htamwjk8d"),
+          infoTitle = "Sample Title",
+          infoInput = "# This is a header\n\nAnd this is a paragraph";
 
 export default class InfoView extends Component {
 
@@ -10,14 +20,36 @@ export default class InfoView extends Component {
 
   }
 
-  updateValue(value) {
-    console.log(this, value)
-    this.setState({value})
-  }
-
   render() {
+
     return (
-      <div>View Info</div>
+      <Paper className="mdEditorPaper" zDepth={2}>
+      <Grid>
+        View Info
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+          <Row>
+          <Col xs={1} />
+          <Col xs={10}>
+            <GridList cellHeight={300} cols={1} style={{ width: '100%', height: 'auto', overflowY: 'auto', marginBottom: 24, paddingLeft:"15px", paddingRight:"15px"}} >
+              <GridTile key={infoDisplayUrl} title={infoTitle} cols={1} >
+                <img src={infoDisplayUrl} />
+              </GridTile>
+            </GridList>
+          </Col>
+          </Row>
+        </div>
+
+        <Markdown source={infoInput} />
+
+        <Row>
+          <Col xs={12} style={{marginTop: "18px"}}>
+            <RaisedButton label="Edit" primary={true} />
+            <RaisedButton label="Share" primary={true} />
+            <RaisedButton label="Like" primary={true} />
+          </Col>
+        </Row>
+      </Grid>
+      </Paper>
     )
   }
 
--- imports/ui/pages/info/view-info.js
+++ imports/ui/pages/info/view-info.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import { Grid } from 'meteor/lifefilm:react-flexbox-grid';
 import InfoView from '../../components/InfoView/InfoView';
 
 const ViewInfo = () => (
-      <Grid>
-        <div>Information Section View</div>
-        <InfoView />
-      </Grid>
+      <InfoView />
     )
 
 export default ViewInfo;
