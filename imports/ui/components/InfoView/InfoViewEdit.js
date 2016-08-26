faaba42c074fae8dea4af37a8c7c84ff90566052
leon mak <leonmakansheng@gmail.com>
2016-08-26 20:44:16 +0800

    Added upload picture & submit functions, edited styles

--- imports/ui/components/InfoView/InfoViewEdit.js
+++ imports/ui/components/InfoView/InfoViewEdit.js
@@ -3,9 +3,11 @@ import ReactMarkdownMediumEditor from 'meteor/universe:react-markdown-wysiwyg/Re
 import { reduxForm, Field } from 'redux-form';
 import { TextField } from 'redux-form-material-ui';
 import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
-import RaisedButton from 'material-ui/RaisedButton';
 import * as IconsHelper from '../../../util/icons';
 import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import LinearProgress from 'material-ui/LinearProgress';
+import { GridList, GridTile } from 'material-ui/GridList';
 
 
 const validate = values => {
@@ -42,14 +44,34 @@ class InfoViewEdit extends Component {
     this.state = {
       value: "# Markdown finally",
       loading: false,
-      tile: null
+      tile: null,
+      title: "Some Title"
     }
   }
 
   componentDidMount() {
     this.refs.title.getRenderedComponent().getRenderedComponent().focus();
   }
+
+  handleUpload(e) {
+    const self = this;
+    const files = e.currentTarget.files;
+    let tile = {};
+    tile.files= files;
+
+    this.setState({ loadingFile: true });
+
+    // upload files to root cloudinary folder
+    Cloudinary.upload(files, {}, function(err, res) {
+      tile.res=res;
+      self.setState({ tile: tile});
+      self.setState({ loadingFile : false})
+    });
+  }
+
   render() {
+    const { handleSubmit, pristine, reset, submitting } = this.props;
+
     return (
       <Grid>
       <form >
@@ -64,55 +86,59 @@ class InfoViewEdit extends Component {
               name="title"
               ref="title"
               style={{marginBottom: "21px"}}
+              value={this.state.value}
+              onBlur={ e => this.setState({title: e.target.value}) }
               withRef
             />
           </Col>
 
           <Col xs={12} md={6}>
             <h1>Upload Image</h1>
-            <RaisedButton className="formBtn" secondary={true} icon={ IconsHelper.materialIcon("backup") } label="Choose an Image">
+            <RaisedButton className="formBtn" rippleStyle={{minHeight: "52px"}} secondary={true} icon={ IconsHelper.materialIcon("backup") } label="Choose an Image">
             <input type="file" accept="image/*"
               style={{ cursor: 'pointer', position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, width: '100%', opacity: 0, zIndex: 1, }}
-              onChange={this.handleUpload} />
+              onChange={this.handleUpload.bind(this)} />
             </RaisedButton>
           </Col>
         </Row>
+      </form>
 
         <Row style={{ paddingBottom: 21 }}>
           <Col xs={12}>
-            {this.state.loadingFile?
-              <LinearProgress mode="indeterminate" id="LinearProgressEdit"/>
-              :
-              <div />
-            }
+            { this.state.loadingFile && <LinearProgress mode="indeterminate" id="LinearProgressEdit"/> }
           </Col>
         </Row>
 
         <Row>
           <Col xs={12}>
-          <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+          <Paper className="mdEditorPaper" zDepth={2}>
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+            <Row>
+            <Col xs={1} />
+            <Col xs={10}>
             { this.state.tile &&
-              <GridList cellHeight={300} cols={2} style={{ width: '100%', height: 'auto', overflowY: 'auto', marginBottom: 24, paddingLeft:"15px", paddingRight:"15px"}} >
+              <GridList cellHeight={300} cols={1} style={{ width: '100%', height: 'auto', overflowY: 'auto', marginBottom: 24, paddingLeft:"15px", paddingRight:"15px"}} >
                 <GridTile
                   key={this.state.tile.res.secure_url}
-                  title={this.state.tile.files[0].name}
-                  cols={2} >
+                  title={this.state.title || this.state.tile.files[0].name}
+                  cols={1} >
                   <img src={this.state.tile.res.secure_url} />
                 </GridTile>
               </GridList>
             }
-          </div>
+            </Col>
+            </Row>
+            </div>
+
+            <ReactMarkdownMediumEditor markdown={ this.state.value } options={options} onChange={console.log.bind(this)} />
+          </Paper>
+          </Col>
+          <Col xs={12} style={{marginTop: "18px"}}>
+            <RaisedButton label="Submit" primary={true} type="submit" disabled={pristine || submitting} />
+            <RaisedButton label="Cancel" primary={true} disabled={pristine || submitting} onClick={reset} />
           </Col>
         </Row>
 
-        <Row id="mdEditor">
-          <Col xs={12}>
-            <Paper zDepth={2}>
-              <ReactMarkdownMediumEditor markdown={ this.state.value } options={options} onChange={console.log.bind(this)} />
-            </Paper>
-          </Col>
-        </Row>
-      </form>
       </Grid>
     )
   }
