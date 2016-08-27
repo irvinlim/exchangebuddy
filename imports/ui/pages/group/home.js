5508ad0e3ace5ae0ab4d4878f69fa038c0523c27
Lam Chi Thanh <E0008973@u.nus.edu>
2016-08-27 11:05:00 +0800

    Add share button to home page

--- imports/ui/pages/group/home.js
+++ imports/ui/pages/group/home.js
@@ -1,16 +1,35 @@
 import React, {PropTypes} from 'react';
+import FlatButton from 'material-ui/FlatButton';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
 import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
 import MemberList from '../../components/Group/Home/MemberList';
 
+const buttonStyle = {
+  textAlign: "center",
+  marginTop: 25
+}
+
 const GroupHome = ({ params }) => (
   <Grid>
     <Row>
       <Col xs={12} style={{ marginTop: 15 }}>
-        <h3 className="pinline"><span>Who's Going</span></h3>
+        <h3 className="pinline" style={{ fontSize: "2em" }}><span>Who's Going</span></h3>
         <MemberList groupId={ params.id } />
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={12} style={{ marginTop: 15 }}>
+        <h3 className="pinline" style={{ fontSize: "1.5em" }}><span>Exchange Buddy Is Better With Friends</span></h3>
+        <Row>
+          <Col xs={12} sm={6} md={6} style={buttonStyle}>
+            <FlatButton label="Share On FaceBook" />
+          </Col>
+          <Col xs={12} sm={6} md={6} style={buttonStyle}>
+            <FlatButton label="Invite Friends" />
+          </Col>
+        </Row>
       </Col>
     </Row>
   </Grid>
