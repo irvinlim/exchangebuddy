e1b651c15b59e7f8375f841c855bdb8af8628795
Irvin Lim <irvinlimweiquan@gmail.com>
2016-08-28 04:28:51 +0800

    Show lesser events on first load, with load more button

--- imports/ui/components/Group/Events/EventList/EventList.js
+++ imports/ui/components/Group/Events/EventList/EventList.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
 import Loading from '../../../Loading';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import EventItemMu from './EventItemMu';
 import EventItemFb from './EventItemFb';
 
 export default class EventsList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showAll: false
+    };
+  }
+
 	render() {
     const { groupEvents, source } = this.props;
 
@@ -17,9 +27,21 @@ export default class EventsList extends React.Component {
         return null;
     };
 
+    const showAllEvents = () => this.setState({ showAll: true });
+
+    // Before button is clicked, only show 5 events
+    const end = this.state.showAll ? groupEvents.length : 5;
+
 		return (
-			<div style={{ height: $(window).height(), overflowY: "auto", overflowX: "hidden" }}>
-        { groupEvents.map((groupEvent, idx) => <EventItem key={ idx } source={ source } groupEvent={ groupEvent } /> ) }
+			<div className="event-list">
+        { groupEvents.slice(0, end).map((groupEvent, idx) => <EventItem key={ idx } source={ source } groupEvent={ groupEvent } /> ) }
+
+        <Row>
+          <Col xs={12}>
+            { this.state.showAll ? null
+              : <RaisedButton label="Show all events" primary={true} style={{ display: "block" }} onTouchTap={ showAllEvents } /> }
+          </Col>
+        </Row>
 			</div>
 		);
 	}
