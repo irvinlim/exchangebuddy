c2f89edbae371dab3a27902a0aa3f785b21c5ef2
Lam Chi Thanh <E0008973@u.nus.edu>
2016-08-28 01:28:18 +0800

    Add share feature on homepage

--- imports/ui/pages/group/home.js
+++ imports/ui/pages/group/home.js
@@ -3,6 +3,7 @@ import FlatButton from 'material-ui/FlatButton';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
+import { FacebookButton, FacebookCount } from 'react-social';
 import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
 import MemberList from '../../components/Group/Home/MemberList';
 
@@ -25,6 +26,9 @@ const GroupHome = ({ params }) => (
         <Row>
           <Col xs={12} sm={6} md={6} style={buttonStyle}>
             <FlatButton label="Share On FaceBook" />
+            <FacebookButton url="http://exchangebuddy.irvinlim.com/" appId={Meteor.settings.public.Facebook.appId}>
+              <FacebookCount url="http://exchangebuddy.irvinlim.com/" /> {"Share on Facebook"}
+            </FacebookButton>
           </Col>
           <Col xs={12} sm={6} md={6} style={buttonStyle}>
             <FlatButton label="Invite Friends" />
