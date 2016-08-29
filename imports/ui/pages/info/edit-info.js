91f7b7836853096b620023677c8d4444aee5a30f
leon mak <leonmakansheng@gmail.com>
2016-08-30 07:49:32 +0800

    Edited info-edit form styles

--- imports/ui/pages/info/edit-info.js
+++ imports/ui/pages/info/edit-info.js
@@ -5,14 +5,20 @@ import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
 import InfoViewEdit from '../../components/InfoView/InfoViewEdit';
 
 const EditInfo = () => (
+  <div className="info-container" >
   <Grid>
     <Row>
       <Col xs={12}>
       <h3 className="pinline"> <span>Edit Information Section</span> </h3>
       </Col>
     </Row>
-    <InfoViewEdit />
+    <Row>
+      <Col xs={12}>
+        <InfoViewEdit />
+      </Col>
+    </Row>
   </Grid>
+  </div>
 )
 
 export default EditInfo;
