4e2583d1c42ead7e7b60cfaa56f6b5e7a4e0ec73
leon mak <leonmakansheng@gmail.com>
2016-08-29 17:21:45 +0800

    Editeed button and edit toolbar styles

--- imports/ui/pages/info/edit-info.js
+++ imports/ui/pages/info/edit-info.js
@@ -5,14 +5,14 @@ import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
 import InfoViewEdit from '../../components/InfoView/InfoViewEdit';
 
 const EditInfo = () => (
-      <Grid>
-        <Row>
-          <Col xs={12}>
-          <h3 className="pinline"> <span>Edit Information Section</span> </h3>
-          </Col>
-        </Row>
-        <InfoViewEdit />
-      </Grid>
-    )
+  <Grid>
+    <Row>
+      <Col xs={12}>
+      <h3 className="pinline"> <span>Edit Information Section</span> </h3>
+      </Col>
+    </Row>
+    <InfoViewEdit />
+  </Grid>
+)
 
 export default EditInfo;
