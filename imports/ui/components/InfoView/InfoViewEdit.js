9c8b9088f4406cb90b085aefa6d2b9b549f55e89
leon mak <leonmakansheng@gmail.com>
2016-08-29 17:58:11 +0800

    Edited medium editor styles

--- imports/ui/components/InfoView/InfoViewEdit.js
+++ imports/ui/components/InfoView/InfoViewEdit.js
@@ -119,7 +119,7 @@ class InfoViewEdit extends Component {
 
         <Row>
           <Col xs={12}>
-          <Paper className="md-editor-paper" zDepth={2}>
+          <Paper className="medium-editor-paper " zDepth={2}>
             <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
             <Row>
             <Col xs={1} />
