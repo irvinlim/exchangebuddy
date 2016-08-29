33bbb8edfe8694408f447de50a9f775f841ef7b3
leon mak <leonmakansheng@gmail.com>
2016-08-29 16:11:45 +0800

    Edited info edit, share button styles

--- imports/ui/components/InfoView/InfoView.js
+++ imports/ui/components/InfoView/InfoView.js
@@ -1,10 +1,17 @@
 import React, {Component, PropTypes} from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
 import Paper from 'material-ui/Paper';
 import { GridList, GridTile } from 'material-ui/GridList';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import * as ImagesHelper from '../../../util/images';
 import Markdown from 'react-markdown';
+import FacebookProvider, { Like } from 'react-facebook';
+import Dialog from 'share-dialog';
+import { browserHistory } from 'react-router';
+import * as IconsHelper from '../../../util/icons';
+import * as ImagesHelper from '../../../util/images';
+
+const FacebookDialog = Dialog.facebook(Meteor.settings.public.Facebook.appId, "http://app.exchangebuddy.com", "http://app.exchangebuddy.com");
 
 const infoDisplayUrl = ImagesHelper.getUrlScale("ikkzaet6oqkqykrarkyg",500),
           infoTitle = "Sample Title",
@@ -24,17 +31,31 @@ export default class InfoView extends Component {
   render() {
 
     return (
-      <Paper zDepth={2}>
+      <Paper zDepth={2} style={{margin: "21px 0"}}>
 
-          <CardMedia className="info-title-container"
-            mediaStyle={{maxHeight: "500px", overflow:"hidden"}}
-            overlay={<CardTitle titleStyle={{lineHeight: "3rem",fontWeight: "400", fontSize:"250%"}} title={infoTitle}
-              subtitleStyle={{fontWeight: "100"}} subtitle={`Updated at: ${lastUpdated}`} />}
-          >
-            <img src={infoDisplayUrl} />
-          </CardMedia>
+        <CardMedia className="info-title-container"
+          mediaStyle={{maxHeight: "500px", overflow:"hidden"}}
+          overlay={<CardTitle titleStyle={{lineHeight: "3rem",fontWeight: "400", fontSize:"250%"}} title={infoTitle}
+            subtitleStyle={{fontWeight: "100"}} subtitle={`Updated at: ${lastUpdated}`} />}
+        >
+          <img src={infoDisplayUrl} />
+        </CardMedia>
 
-          <Markdown className="md-info" source={infoInput} />
+        <Markdown className="md-info" source={infoInput} />
+
+        <div className="row center-xs" >
+          <Col xs={3} >
+            <RaisedButton label="Edit" className="raised-btn" fullWidth={true} primary={true} icon={IconsHelper.materialIcon('mode_edit')} onTouchTap={ () => browserHistory.push(`${window.location.pathname}/edit`) }/>
+          </Col>
+          <Col xs={3}>
+            <RaisedButton label="Share" className="raised-btn" fullWidth={true} primary={true} icon={IconsHelper.icon('fa fa-facebook-f', {color: "#FFFFFF"})} onTouchTap={ () => FacebookDialog.open() }/>
+          </Col>
+          <Col xs={3} style={{margin: "21px 0"}}>
+            <FacebookProvider appID={Meteor.settings.public.Facebook.appId} >
+              <Like href="http://www.exchangebuddy.com" colorScheme="dark" showFaces />
+            </FacebookProvider>
+          </Col>
+        </div>
 
       </Paper>
     )
--- imports/ui/pages/info/view-info.js
+++ imports/ui/pages/info/view-info.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
 import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
-import RaisedButton from 'material-ui/RaisedButton';
 import InfoView from '../../components/InfoView/InfoView';
-import FacebookProvider, { Like } from 'react-facebook';
-import Dialog from 'share-dialog';
-import * as IconsHelper from '../../../util/icons';
-
-const FacebookDialog = Dialog.facebook(Meteor.settings.public.Facebook.appId, "http://exchangebuddy.irvinlim.com", "http://exchangebuddy.irvinlim.com");
 
 const ViewInfo = () => (
   <Grid>
@@ -16,19 +9,6 @@ const ViewInfo = () => (
         <InfoView />
       </Col>
     </Row>
-    <div className="row center-xs" style={{margin: "21px 0"}}>
-      <Col xs={2}>
-        <RaisedButton label="Edit" primary={true} icon={IconsHelper.materialIcon('mode_edit')} onTouchTap={ () => browserHistory.push(`${window.location.pathname}/edit`) }/>
-      </Col>
-      <Col xs={2}>
-        <RaisedButton label="Share" primary={true} icon={IconsHelper.icon('fa fa-facebook-f', {color: "#FFFFFF"})} onTouchTap={ () => FacebookDialog.open() }/>
-      </Col>
-      <Col xs={2}>
-        <FacebookProvider appID={Meteor.settings.public.Facebook.appId} >
-          <Like href="http://www.exchangebuddy.com" colorScheme="dark" showFaces />
-        </FacebookProvider>
-      </Col>
-    </div>
   </Grid>
 )
 
