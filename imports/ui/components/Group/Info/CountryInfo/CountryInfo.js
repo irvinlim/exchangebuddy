5e9a8e719749f4acfd63feb43406fe8618dd807e
Irvin Lim <irvinlimweiquan@gmail.com>
2016-08-30 10:25:33 +0800

    Fix URL route for info item, remove redundant files

--- imports/ui/components/Group/Info/CountryInfo/CountryInfo.js
+++
@@ -1,6 +0,0 @@
-const CountryInfo = ({ country, sectionItems }) => (
-  <div>
-    <h3 className="pinline"><span>About { country.name }</span></h3>
-    { sectionItems.map(country => ) }
-  </div>
-);
