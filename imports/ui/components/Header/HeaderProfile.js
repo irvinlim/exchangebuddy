44ab35c0ad917e9351b93cc734975acb21974a81
Irvin Lim <irvinlimweiquan@gmail.com>
2016-09-01 16:24:38 +0800

    Add default group cover photo, fix HeaderProfile

--- imports/ui/components/Header/HeaderProfile.js
+++ imports/ui/components/Header/HeaderProfile.js
@@ -68,7 +68,7 @@ export default class HeaderProfile extends React.Component {
 
           <List>
             <ListItem
-              innerDivStyle={{ lineHeight: "20px", paddingLeft: 90, paddingBottom: 20 }}
+              innerDivStyle={{ lineHeight: "20px", height: 60, paddingLeft: 90, paddingTop: 16 }}
               leftAvatar={ UniversityHelper.getImage(uni, cSize) }
               primaryText={ <strong>{ uni.name }</strong> }
               secondaryText={ `${group.term} ${group.year}` }
@@ -82,7 +82,7 @@ export default class HeaderProfile extends React.Component {
             <Divider />
 
             <ListItem
-              innerDivStyle={{ lineHeight: "20px", paddingLeft: 90, paddingBottom: 20 }}
+              innerDivStyle={{ lineHeight: "20px", height: 60, paddingLeft: 90, paddingTop: 16 }}
               leftAvatar={ UserHelper.getAvatar(user, cSize) }
               primaryText={ <strong>{ user.displayName }</strong> }
               secondaryText={ user.homeUniversity.name }
