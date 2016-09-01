f639c508e340336170c1182192a743a41af48323
leon mak <leonmakansheng@gmail.com>
2016-09-01 19:35:32 +0800

    Added basic unapproved action(story) facebook

--- imports/ui/pages/group/home.js
+++ imports/ui/pages/group/home.js
@@ -5,12 +5,38 @@ import MemberList from '../../components/Group/Home/MemberList';
 import FacebookShare from '../../components/Button/FacebookShare';
 import RaisedButton from 'material-ui/RaisedButton';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { showSnackbar } from '../../../client/actions/snackbar';
+
+
 const buttonStyle = {
   textAlign: "center",
   marginTop: 25
 };
 
-const GroupHome = ({ params }) => (
+const joinedGroupStory = (params, actions) => () => {
+  FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+    var uid = response.authResponse.userID;
+    var accessToken = response.authResponse.accessToken;
+    FB.api('me/exchangebuddy:join', 'post',
+      {
+        'access_token': accessToken,
+        'exchange_group': window.location.href,
+        'explicitly_shared': true,
+      },
+      function(response) { actions.showSnackbar("Published event on facebook."); }
+    );
+  } else if (response.status === 'not_authorized') {
+    actions.showSnackbar("You must authorize the application.")
+  } else {
+    actions.showSnackbar("You must be log in before inviting.");
+  }
+ });
+}
+
+const GroupHome = ({ params, actions }) => (
   <Grid>
     <Row>
       <Col xs={12} style={{ marginTop: 15 }}>
@@ -26,7 +52,7 @@ const GroupHome = ({ params }) => (
             <FacebookShare groupId={ params.id } />
           </Col>
           <Col xs={12} sm={6} md={6} style={buttonStyle}>
-            <RaisedButton primary={true} label="Invite Friends" />
+            <RaisedButton primary={true} label="Invite Friends" onTouchTap={ joinedGroupStory(params, actions) }/>
           </Col>
         </Row>
       </Col>
@@ -34,4 +60,10 @@ const GroupHome = ({ params }) => (
   </Grid>
 );
 
-export default GroupHome;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators({ showSnackbar }, dispatch),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(GroupHome);
