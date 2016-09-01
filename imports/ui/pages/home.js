132f9914eb21fc81491e52c7d02ef6d52b52cbd7
leon mak <leonmakansheng@gmail.com>
2016-09-01 22:19:09 +0800

    Added landing page style

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -5,34 +5,32 @@ import * as IconsHelper from '../../util/icons';
 import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
 import {Tabs, Tab} from 'material-ui/Tabs';
 
-const welcomeStyle = { height: $(window).height(), backgroundColor: "darkslategray" }
+const landingContainerStyle = {
+  height: $(window).height(),
+  backgroundColor: "darkslategray",
+}
+
+const landingImg = {
+  background: `linear-gradient(to top, rgba(25, 25, 25, 0.21) 0%,rgb(0, 0, 0) 215%),
+    url(${ImagesHelper.getUrlScale(Meteor.settings.public.landingImageId, 900)}) no-repeat center center `,
+  backgroundSize: "cover"
+}
 
 const Home = () => (
-  <div className="text-center" style={welcomeStyle}>
-    <div id="welcome-header">
+  <div id="landing-container" style={landingContainerStyle}>
+    <div id="welcome-header" style={landingImg}>
 
       <div id="logo-image">
-        { ImagesHelper.makeScale(Meteor.settings.public.logoImageId, 99, "exchangebuddy-logo") }
+        { ImagesHelper.makeScale(Meteor.settings.public.logoImageId, 150, "exchangebuddy-logo") }
       </div>
 
-      <Grid>
-        <Row>
-          <Col xs={0} md={2}></Col>
-          <Col xs={12} md={8} id="welcome-header-title">
-            <h2 id="app-name">Exchange Buddy</h2>
-            <p id="app-description">Find other exchange students like yourself</p>
-          </Col>
-        </Row>
-      </Grid>
-    </div>
+      <div id="welcome-header-title">
+        <h2 id="app-title">Find out who else is on an adventure</h2>
+        <p className="app-subtitle">Share tips for the trip, find travel buddies, by students, for students. </p>
+        <p className="app-subtitle">Forget the random facebook groups and google forms, all you need is right here. </p>
+      </div>
 
-    <div id="welcome-body">
-      <div id="welcome-body-message">Share tips,</div>
-      <div id="welcome-body-message">chat about adventures,</div>
-      <div id="welcome-body-message">stay updated with the latest news.</div>
-    </div>
-    <div id="login-button">
-      <LoginButton />
+      <div id="login-button"><LoginButton /></div>
     </div>
   </div>
 );
