c21ec441e513edc9f0a31813ee4727b8301c1c6b
Irvin Lim <irvinlimweiquan@gmail.com>
2016-09-01 13:24:52 +0800

    Cleanup profile page

--- imports/ui/components/ProfilePaper/ProfilePaper.js
+++ imports/ui/components/ProfilePaper/ProfilePaper.js
@@ -6,29 +6,9 @@ import Link from '../Link';
 
 import * as UserHelper from '../../../util/user';
 
-const table_style = {
-  width: "90%",
-  tableLayout: "fixed"
-};
-
-const table_header_style = {
-  fontSize: "medium",
-  fontWeight: 500,
-  width: "35%"
-};
-
-const table_body_style = {
-  fontSize: "medium",
-  fontWeight: "light",
-  color: "gray",
-  width: "65%"
-};
-
 const text_header_style = {
   fontSize: "-webkit-xxx-large",
   fontWeight: 300,
-  textAlign: "left",
-  paddingLeft: "4%"
 };
 
 export default class ProfilePaper extends React.Component {
@@ -44,34 +24,32 @@ export default class ProfilePaper extends React.Component {
             </Col>
 
             <Col xs={12} md={7} id="user-info">
-              <h1 style={text_header_style}>{ user.displayName }</h1>
-              <div><hr style={{ width: "85%", paddingLeft: "4%" }}/></div>
-              <Table selectable={false} style={table_style}>
-                <TableBody displayRowCheckbox={false}>
+              <h1 style={ text_header_style }>{ user.displayName }</h1>
 
-                  <TableRow displayBorder={false}>
-                    <TableRowColumn style={table_header_style}>HOME UNI</TableRowColumn>
-                    <TableRowColumn style={table_body_style}>
+              <div className="flex-table-container">
+
+                  <Row>
+                    <Col xs={12} sm={4} className="table-header">Home University</Col>
+                    <Col xs={12} sm={8} className="table-cell">
                       { userHomeUniversity.name }
-                    </TableRowColumn>
-                  </TableRow>
+                    </Col>
+                  </Row>
 
-                  <TableRow displayBorder={false}>
-                    <TableRowColumn style={table_header_style}>ON EXCHANGE TO</TableRowColumn>
-                    <TableRowColumn style={table_body_style}>
+                  <Row>
+                    <Col xs={12} sm={4} className="table-header">On exchange to</Col>
+                    <Col xs={12} sm={8} className="table-cell">
                       { userExchangeUniversities.map(uni => <p>{ uni.name }</p>) }
-                    </TableRowColumn>
-                  </TableRow>
+                    </Col>
+                  </Row>
 
-                  <TableRow displayBorder={false}>
-                    <TableRowColumn style={table_header_style}>FACEBOOK</TableRowColumn>
-                    <TableRowColumn style={table_body_style}>
+                  <Row>
+                    <Col xs={12} sm={4} className="table-header">Facebook</Col>
+                    <Col xs={12} sm={8} className="table-cell">
                       <Link to={ `https://facebook.com/${user.fbUserId}` }>Facebook profile</Link>
-                    </TableRowColumn>
-                  </TableRow>
+                    </Col>
+                  </Row>
 
-                </TableBody>
-              </Table>
+              </div>
             </Col>
           </Row>
         </Grid>
