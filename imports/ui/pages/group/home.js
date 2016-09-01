fff24201e92ac8158cf3b353783bd1e4c2bc87d5
Irvin Lim <irvinlimweiquan@gmail.com>
2016-09-02 03:47:59 +0800

    Added GroupWelcome box

--- imports/ui/pages/group/home.js
+++ imports/ui/pages/group/home.js
@@ -1,6 +1,7 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 import { Grid, Row, Col } from 'meteor/lifefilm:react-flexbox-grid';
 
+import GroupWelcome from '../../components/Group/Home/GroupWelcome';
 import MemberList from '../../components/Group/Home/MemberList';
 import FacebookShare from '../../components/Button/FacebookShare';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -38,25 +39,36 @@ const joinedGroupStory = (params, actions) => () => {
 
 const GroupHome = ({ params, actions }) => (
   <Grid>
+
     <Row>
-      <Col xs={12} style={{ marginTop: 15 }}>
-        <h3 className="pinline" style={{ fontSize: "2em" }}><span>Who's going</span></h3>
-        <MemberList groupId={ params.id } />
+      <Col xs={12}>
+        <GroupWelcome groupId={ parseInt(params.id) } />
       </Col>
     </Row>
+
     <Row>
       <Col xs={12} style={{ marginTop: 15 }}>
-        <h3 className="pinline" style={{ fontSize: "1.5em" }}><span>ExchangeBuddy is better with friends</span></h3>
+        <h3 className="pinline" style={{ fontSize: "1.3em" }}><span>Who's going</span></h3>
+        <p className="small-text" style={{ textAlign: 'center' }}>These students will be on exchange at the same time as you.</p>
+        <MemberList groupId={ parseInt(params.id) } />
+      </Col>
+    </Row>
+
+    <Row>
+      <Col xs={12} style={{ marginTop: 15 }}>
+        <h3 className="pinline" style={{ fontSize: "1.3em" }}><span>ExchangeBuddy is better with friends</span></h3>
+        <p className="small-text" style={{ textAlign: 'center' }}>Get your friends to join in the fun!</p>
         <Row>
-          <Col xs={12} sm={6} md={6} style={buttonStyle}>
+          <Col xs={6} style={buttonStyle}>
             <FacebookShare groupId={ params.id } />
           </Col>
-          <Col xs={12} sm={6} md={6} style={buttonStyle}>
+          <Col xs={6} style={buttonStyle}>
             <RaisedButton primary={true} label="Invite Friends" onTouchTap={ joinedGroupStory(params, actions) }/>
           </Col>
         </Row>
       </Col>
     </Row>
+
   </Grid>
 );
 
