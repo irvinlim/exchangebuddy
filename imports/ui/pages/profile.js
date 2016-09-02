c4969e7a365067032159b30cf7c5eda6a7b9d5bf
Lam Chi Thanh <E0008973@u.nus.edu>
2016-09-02 16:09:53 +0800

    Add links to profile page

--- imports/ui/pages/profile.js
+++ imports/ui/pages/profile.js
@@ -6,15 +6,31 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 
 import ProfilePaper from '../components/ProfilePaper';
 
+const landingImg = {
+  background: `linear-gradient(to top, rgba(25, 25, 25, 0.21) 0%,rgb(0, 0, 0) 215%),
+    url(${ImagesHelper.getUrlScale(Meteor.settings.public.landingImageId, 900)}) no-repeat center center `
+}
+
 const Profile = ({ params }) => (
-  <div className="page-profile">
-    <Grid>
-      <Row>
-        <Col xs={12}>
-          <ProfilePaper userId={ params.userId } />
-        </Col>
-      </Row>
-    </Grid>
+  <div id="welcome-header" style={landingImg}>
+    <div id="logo-image" style={{paddingLeft: "4%"}}>
+      { ImagesHelper.makeScale(Meteor.settings.public.logoImageId, 150, "exchangebuddy-logo") }
+    </div>
+    <div id="navigation-link" style={{paddingLeft: "50%", paddingTop: "4%"}}>
+      <a href="www.google.com" className="link">Home</a>
+      <a href="" className="link">About</a>
+      <a href="" className="link">Privacy Policy</a>
+    </div>
+
+    <div style={{paddingTop: "150px"}}>
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <ProfilePaper userId={ params.userId } />
+          </Col>
+        </Row>
+      </Grid>
+    </div>
   </div>
 );
 
