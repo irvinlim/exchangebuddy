fd92bf99d12662f7a6d9ceb2ab6b9e2e32a50b87
Lam Chi Thanh <E0008973@u.nus.edu>
2016-09-02 20:03:45 +0800

    Add about info

--- imports/ui/pages/profile.js
+++ imports/ui/pages/profile.js
@@ -9,11 +9,11 @@ import NavigationBar from '../components/NavigationBar/NavigationBar.js';
 
 const landingImg = {
   background: `linear-gradient(to top, rgba(25, 25, 25, 0.21) 0%,rgb(0, 0, 0) 215%),
-    url(${ImagesHelper.getUrlScale(Meteor.settings.public.landingImageId, 900)}) no-repeat center top `
+    url(${ImagesHelper.getUrlScale('exchangebuddy/norway', 900)}) no-repeat center top `
 }
 
 const Profile = ({ params }) => (
-  <div id="half-header" style={landingImg}>
+  <div id="welcome-header" style={landingImg}>
     <NavigationBar />
 
     <div style={{paddingTop: "150px"}}>
