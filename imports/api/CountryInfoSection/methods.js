8a46f8207dfc1dbb5bb20296b55fb753b0947c95
Irvin Lim <irvinlimweiquan@gmail.com>
2016-09-02 18:14:51 +0800

    Bugfix in CountryInfoSection.get method

--- imports/api/CountryInfoSection/methods.js
+++ imports/api/CountryInfoSection/methods.js
@@ -4,10 +4,10 @@ import CountryInfoSection from '.';
 if (Meteor.isServer) {
   Meteor.methods({
 
-    'CountryInfoSection.get'(countryCode) {
-      check(countryCode, String);
+    'CountryInfoSection.get'(id) {
+      check(id, Number);
 
-      return CountryInfoSection.findById(countryCode).then(function(result) {
+      return CountryInfoSection.findById(id).then(function(result) {
         const section = result && result.get({ plain: true });
         return section;
       });
